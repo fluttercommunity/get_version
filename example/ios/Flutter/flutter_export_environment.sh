f50c2587b14b29982f067ee9ad8f3e1494835224
HH <hhsc2004@gmail.com>
2022-11-15 00:36:56 +0800

    Upgrade kotlin version

--- example/ios/Flutter/flutter_export_environment.sh
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -1,15 +1,13 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-
-export "FLUTTER_ROOT=/usr/local/Caskroom/flutter/1.2.1/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/rodydavis/Developer/GitHub/plugins/packages/get_version/example"
+export "FLUTTER_ROOT=D:\Software\SDK\flutter"
+export "FLUTTER_APPLICATION_PATH=F:\WorkSet\Other\get_version\example"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
-export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
-export "SYMROOT=${SOURCE_ROOT}/../build\ios"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
 export "DART_OBFUSCATION=false"
-export "TRACK_WIDGET_CREATION=false"
+export "TRACK_WIDGET_CREATION=true"
 export "TREE_SHAKE_ICONS=false"
-export "PACKAGE_CONFIG=.packages"
+export "PACKAGE_CONFIG=.dart_tool/package_config.json"
